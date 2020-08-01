a7d877ace714c989314d00270b75c5d9573ab7a3
Alok Kumar <alokkola21@gmail.com>
2020-08-02 00:55:47 +0530

    completed

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("search");
+});
+
+app.get("/results", function (req, res) {
+  var query = req.query.search;
+  request("http://api.tvmaze.com/search/shows?q=" + query, function (
+    error,
+    response,
+    body
+  ) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      res.render("results", { data: data });
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("server started");
+});
